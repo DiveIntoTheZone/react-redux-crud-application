78967d4b0dff50d5445bc325cc9ea3cc644edd81
kazuhiko1979 <challengevolley1979@gmail.com>
2023-07-26 14:33:41 +0900

    implement store

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
-import App from './App';
+import reducer from './reducers';
+import App from '.components/App';
 import reportWebVitals from './reportWebVitals';
+import reducers from './reducers';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+const store = createStore(reducers)
+
+ReactDOM.createRoot(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  </Provider>,
+  document.getElementById('root'));
 reportWebVitals();
+
+
+// ReactDOM.createRoot(document.getElementById('root'));
+// reportWebVitals();
