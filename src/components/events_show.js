9d58525dd6c662d7fc06b213af092f0608f7b916
kazuhiko1979 <challengevolley1979@gmail.com>
2023-08-09 13:34:44 +0900

    implement delete feature

--- src/components/events_show.js
+++ src/components/events_show.js
@@ -2,13 +2,21 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux' 
 import { Field, reduxForm } from 'redux-form'
 import { Link } from 'react-router-dom'
-import { postEvent } from '../actions'
 
-class EventsNew extends Component {
+import { getEvent, deleteEvent, putEvent } from '../actions'
+
+class EventsShow extends Component {
   constructor(props) {
     super(props)
     this.onSubmit = this.onSubmit.bind(this)
+    this.onDeleteClick = this.onDeleteClick.bind(this)
   }
+
+  componentDidMount() {
+    const { id } = this.props.match.params
+    if (id) this.props.getEvent(id)
+  }
+
   renderField(field) {
     const { input, label, type, meta: { touched, error } } = field
 
@@ -20,8 +28,15 @@ class EventsNew extends Component {
     )
   }
 
+  async onDeleteClick() {
+    const { id } = this.props.match.params
+    console.log(id)
+    await this.props.deleteEvent(id)
+    this.props.history.push('/')
+  }
+
   async onSubmit(values) {
-    await this.props.postEvent(values)
+    await this.props.putEvent(values)
     this.props.history.push('/')
   }
 
@@ -37,8 +52,10 @@ class EventsNew extends Component {
         </div>
       
         <div>
-          <input type="submit" value="Submit" disabled={pristine || submitting || invalid} />
+          <input type="submit" value="Submit" disabled={ pristine || submitting || invalid} />
           <Link to="/">Cancel</Link>
+          <Link to="/" onClick={this.onDeleteClick} >Delete</Link>
+          
         </div>
       </form>
     )
@@ -54,10 +71,15 @@ const validate = values => {
   return errors 
 }
 
-const mapDispatchToProps = ({ postEvent })
 
+const mapStateToProps = (state, ownProps) => {
+  const event = state.events[ownProps.match.params.id]
+  return { initialValues: event, event }
+}
+const mapDispatchToProps = ({ deleteEvent, getEvent, putEvent })
 
-export default connect(null, mapDispatchToProps )(
-  reduxForm({ validate, form: 'eventNewForm' })(EventsNew)
+export default connect(mapStateToProps, mapDispatchToProps)(
+  reduxForm({ validate, form: 'eventShowForm', enableReinitialize: true })(EventsShow)
 )
+
 
--- src/index.js
+++ src/index.js
@@ -1,29 +1,45 @@
 import { React } from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+// import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { BrowserRouter, Route, Routes }  from 'react-router-dom';
+import { configureStore } from '@reduxjs/toolkit';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import './index.css';
 // import reducer from './reducers';
 import rootReducer from './reducers';
 import EventsIndex from './components/events_index';
 import EventsNew from './components/events_new';
+import EventsShow from './components/events_show';
 import reportWebVitals from './reportWebVitals';
 
-// const store = createStore(reducer, applyMiddleware(thunk))
-const store = createStore(rootReducer, applyMiddleware(thunk));
 
+// const enhancer = process.env.NODE_ENV === 'development' ? 
+//   composeWithDevTools(applyMiddleware(thunk)): applyMiddleware(thunk)
+
+// const store = createStore(rootReducer, enhancer);
+
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(thunk),
+  devTools: process.env.NODE_ENV !== 'production',
+});
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/events/new" element={<EventsNew />} /> 
-        <Route path="/" element={<EventsIndex />} />
-      </Routes>
-    </BrowserRouter>
-  </Provider>,
+  <MuiThemeProvider>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/events/new" element={<EventsNew />} /> 
+          <Route path="/events/:id" element={<EventsShow />} /> 
+          <Route path="/" element={<EventsIndex />} />
+          <Route path="/events" element={<EventsIndex />} /> 
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  </MuiThemeProvider>,
   document.getElementById('root'));
 reportWebVitals();
